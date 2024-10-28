90de1438fbe3052cc6c94c8a122e77d0d131b955
Grégoire Pineau <lyrixx@lyrixx.info>
2024-10-28 18:13:17 +0100

    Increase the version of ext-rdkafka (fake) installed in CI

--- Tests/fix_composer_json.php
+++ Tests/fix_composer_json.php
@@ -5,7 +5,7 @@ copy(__DIR__.'/../composer.json', __DIR__.'/../composer.json.bak');
 $composerJson = json_decode(file_get_contents(__DIR__.'/../composer.json'), true);
 
 $composerJson['config']['platform']['ext-amqp'] = '1.9.3';
-$composerJson['config']['platform']['ext-rdkafka'] = '3.3';
+$composerJson['config']['platform']['ext-rdkafka'] = '6';
 $composerJson['config']['platform']['ext-gearman'] = '2';
 
 file_put_contents(__DIR__.'/../composer.json', json_encode($composerJson, JSON_PRETTY_PRINT));
